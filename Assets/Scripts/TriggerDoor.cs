5aa2c0fe2357905021c92dba41ded8ae55b3f65d
DESKTOP-I8ROVPA\Vanessa <gamejam@einrot.com>
2020-12-12 13:44:41 +0100

    Trigger Door -> switch level 1 zu level 2

---
+++ Assets/Scripts/TriggerDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class TriggerDoor : MonoBehaviour
+{
+    void Start()
+    {
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "myassetBundle"));
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+
+        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("MyObject");
+        Instantiate(prefab);
+
+        myLoadedAssetBundle.Unload(false);
+    }
+
+
+    void OnTriggerEnter(Collider doorInformation)
+    {
+        if (doorInformation.gameObject.name == "Player")
+        {
+            Debug.Log("Collision Detected");
+            SceneManager.LoadScene("TestLevel02", LoadSceneMode.Single); 
+        }
+    }
+}

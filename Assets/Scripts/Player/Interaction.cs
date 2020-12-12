d8b262b921705aa9aab7ac02b6a0c5db9a72296d
Lukas <Bone008@users.noreply.github.com>
2020-12-12 03:44:31 +0100

    Add button that you can activate by pressing F.

---
+++ Assets/Scripts/Player/Interaction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    public LayerMask interactableLayer;
+    public float interactionRange;
+
+    private GameObject currentTarget = null;
+
+    void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, interactionRange, interactableLayer, QueryTriggerInteraction.Collide))
+        {
+            SetTarget(hit.collider.gameObject);
+        }
+        else
+        {
+            SetTarget(null);
+        }
+
+        if(Input.GetButtonDown("Interact") && currentTarget != null)
+        {
+            currentTarget.SendMessage("Activate");
+        }
+    }
+
+    private void SetTarget(GameObject newTarget)
+    {
+        if (currentTarget == newTarget) return;
+        currentTarget = newTarget;
+        Debug.Log("Now targeting: " + currentTarget, this);
+        // TODO: Highlight what is being targeted somehow.
+    }
+}
---
+++ Assets/Scripts/TriggerButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerButton : MonoBehaviour
+{
+    public MechanismBase targetMechanism;
+    public float activationDuration = 2f;
+    public MechanismBase buttonFaceMechanism;
+
+    private Coroutine resetCoroutine = null;
+
+    public void Activate()
+    {
+        Debug.Log($"Activating button for {activationDuration:0.#} s!", this);
+        targetMechanism?.SetTriggerActive(true);
+        buttonFaceMechanism.SetTriggerActive(true);
+        if (resetCoroutine != null)
+        {
+            StopCoroutine(resetCoroutine);
+        }
+        resetCoroutine = StartCoroutine(DoReset());
+    }
+
+    private IEnumerator DoReset()
+    {
+        yield return new WaitForSeconds(activationDuration);
+        Debug.Log("Deactivating button!", this);
+        targetMechanism?.SetTriggerActive(false);
+        buttonFaceMechanism.SetTriggerActive(false);
+    }
+}

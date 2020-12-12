291fd73635d8c7dfe00afa62070e10fa2f7b301f
Lukas <Bone008@users.noreply.github.com>
2020-12-12 14:07:44 +0100

    Add fast forward for debugging because time is precious

---
+++ Assets/Scripts/DebugFastForward.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugFastForward : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            Time.timeScale = 5f;
+        }
+        else if (Input.GetKeyUp(KeyCode.F3))
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}

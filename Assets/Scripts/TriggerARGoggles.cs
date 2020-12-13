3c72ccd5a450a9a7f095981c24b7547a2416104d
Lukas <Bone008@users.noreply.github.com>
2020-12-13 01:57:02 +0100

    Make level 0 super dooper nice

--- Assets/Scripts/TriggerARGoggles.cs
+++ Assets/Scripts/TriggerARGoggles.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
+using UnityEngine.UI;
 
 public class TriggerARGoggles : MonoBehaviour
 {
     public GameObject hudGameObject;
     public MechanismTransform hideExitMechanism;
     private Animator animator;
+    public PostProcessingBehaviour postProcessing;
+    public GameObject[] stuffToDisableOnPickup;
+    public GameObject[] stuffToEnableForAR;
+
+    public Image uiImageToMakeHologram;
+    public Material hologramMaterial;
+
+    public GameObject initializingHud;
 
     void Awake()
     {
         animator = GetComponentInChildren<Animator>();
         
         hudGameObject.SetActive(false);
+        postProcessing.enabled = false;
     }
     public void Activate()
     {
@@ -27,6 +38,9 @@ public class TriggerARGoggles : MonoBehaviour
 
         GetComponent<Collider>().enabled = false;
 
+        foreach (var go in stuffToDisableOnPickup)
+            go.SetActive(false);
+
         animator.SetTrigger("activate");
         yield return new WaitForSeconds(1f);
         while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f)
@@ -34,8 +48,26 @@ public class TriggerARGoggles : MonoBehaviour
 
         // Activate HUD
         hudGameObject.SetActive(true);
-        gameObject.SetActive(false);
+        postProcessing.enabled = true;
+        uiImageToMakeHologram.material = hologramMaterial;
+
         // Show exit door.
         hideExitMechanism.SetTriggerActive(false);
+
+        foreach (var go in stuffToEnableForAR)
+            go.SetActive(true);
+
+        initializingHud.SetActive(true);
+        var initText = initializingHud.GetComponentInChildren<Text>();
+        string fullStr = initText.text;
+        for(int i=1; i<fullStr.Length; i++)
+        {
+            initText.text = fullStr.Substring(0, i);
+            yield return new WaitForSeconds(0.09f);
+        }
+        yield return new WaitForSeconds(2f);
+        initializingHud.SetActive(false);
+
+        gameObject.SetActive(false);
     }
 }
